56b75ce786f2901d6f636cf4c5ea6b15c91f2130
William Gregory Dallaway <122135755+Will-D2099@users.noreply.github.com>
2024-01-18 14:06:13 +0100

    updating NuGraph with detector features

--- nugraph/models/NuGraph2.py
+++ nugraph/models/NuGraph2.py
@@ -21,12 +21,10 @@ class NuGraph2(LightningModule):
     Wrap the base model in a LightningModule wrapper to handle training and
     inference, and compute training metrics."""
     def __init__(self,
-                 in_features: int = 4,
+                 in_features: int = 8,
                  planar_features: int = 64,
                  nexus_features: int = 16,
-                 vertex_aggr: str = 'lstm',
-                 vertex_lstm_features: int = 64,
-                 vertex_mlp_features: list[int] = [ 64 ],
+                 vertex_features: int = 32,
                  planes: list[str] = ['u','v','y'],
                  semantic_classes: list[str] = ['MIP','HIP','shower','michel','diffuse'],
                  event_classes: list[str] = ['numu','nue','nc'],
@@ -93,9 +91,7 @@ class NuGraph2(LightningModule):
         if vertex_head:
             self.vertex_decoder = VertexDecoder(
                 planar_features,
-                vertex_aggr,
-                vertex_lstm_features,
-                vertex_mlp_features,
+                vertex_features,
                 planes,
                 semantic_classes)
             self.decoders.append(self.vertex_decoder)
@@ -266,11 +262,7 @@ class NuGraph2(LightningModule):
                            help='Hidden dimensionality of planar convolutions')
         model.add_argument('--nexus-feats', type=int, default=16,
                            help='Hidden dimensionality of nexus convolutions')
-        model.add_argument('--vertex-aggr', type=str, default='lstm',
-                           help='Aggregation function for vertex decoder')
-        model.add_argument('--vertex-lstm-feats', type=int, default=32,
-                           help='Hidden dimensionality of vertex LSTM aggregation')
-        model.add_argument('--vertex-mlp-feats', type=int, nargs='*', default=[32],
+        model.add_argument('--vertex-feats', type=int, default=32,
                            help='Hidden dimensionality of vertex decoder')
         model.add_argument('--event', action='store_true', default=False,
                            help='Enable event classification head')
